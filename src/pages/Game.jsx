248579e95959bf1ce8a674cabc7469ce51b320e2
Elliot Erickson <elliot_erickson@protonmail.com>
2023-01-18 11:29:52 -0800

    Game control Images, Card image
    Added button images from the Figma for the game controls. They do not have the border shadow, but the shadow is in the image. Added a random image to configure the image section of the game card element

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -4,25 +4,38 @@ function Game () {
 
     return(
         <div className='container mx-auto h-screen'>
-            <div className='m-1 border-2 border-black h-4/6 mb-6 mt-12'>
-                <div className='h-3/5'>
-                
-                Card Window
+            <div className='m-1 border-4 border-black h-4/6 mb-6 mt-12 mx-3'>
+                <div className='h-3/5 flex justify-center'>
+                    <img className='flex-none m-1' src='src\assets\card3.jpg' alt='Three' />
                 </div>
                 <div className='flex flex-col h-2/5 justify-center px-12'>
-                    <button type='button' className='btn-answer-primary '>
-                        one
+                    <button type='button' className='btn-answer '>
+                        <span className='btn-answer-text'>
+                            ONE
+                        </span>
                     </button>
-                    <button type='button' className='btn-answer-primary'>
-                        two
+                    <button type='button' className='btn-answer'>
+                        <span className='btn-answer-text'>
+                            TWO
+                        </span>
                     </button>
-                    <button type='button' className='btn-answer-primary'>
-                        three
+                    <button type='button' className='btn-answer'>
+                        <span className='btn-answer-text'>
+                            THREE
+                        </span>
                     </button>
                 </div>
             </div>
-            <div className='border-2 border-black h-1/6 m-1'>
-                Game Controls
+            <div className='flex justify-around items-center h-1/6 m-1'>
+                <button type='button' className=''>
+                    <img className='flex-none' src='src\assets\btn-skip-left.png' />
+                </button>
+                <button type='button' className=''>
+                    <img className='flex-none' src='src\assets\btn-more-options.png' />
+                </button>
+                <button type='button' className=''>
+                    <img className='flex-none' src='src\assets\btn-skip-right.png' />
+                </button>
             </div>
         </div>
     )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    borderWidth: {
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+    },
     extend: {
       colors: {
         turquoise: "#16ABAC",
