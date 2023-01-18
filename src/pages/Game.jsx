328c63ac810518bd42f89755c05639ca6a98db32
Elliot Erickson <elliot_erickson@protonmail.com>
2023-01-18 13:22:09 -0800

    responsive design
    Added breakpoints for medium screen, control buttons change position to either side of the card (with option button inside).

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -6,17 +6,22 @@ function Game () {
         <div className='container mx-auto h-full '>
             <div className='m-1 border-4 border-black h-4/6 mb-6 mt-12 mx-3 md:border-0' >
                 <div className='md:flex'>
-                <button type='button' className='hidden md:block md:flex-grow-3'>
+                <button type='button' className='hidden md:block md:mr-1'>
                     <img className='flex-none' src='src\assets\btn-skip-left.png' />
                 </button>
-                <div className='flex justify-center md:border-4 md:border-black'>
-                    <img className='flex-none m-1' src='src\assets\card3.jpg' alt='Three' />
+                <div className='flex justify-center md:border-4 md:border-black md:flex-grow md:h-96'>
+                    <div className='m-1 md:flex-1 md:flex md:justify-center'>
+                        <img className='' src='src\assets\card3.jpg' alt='Three' />
+                    </div>
+                    <button type='button' className='hidden md:flex md:mr-1 md:mt-1'>
+                        <img className='' src='src\assets\btn-more-options.png' />
+                    </button>
                 </div>
-                <button type='button' className='hidden md:block'>
+                <button type='button' className='hidden md:block md:ml-1'>
                 <img className='flex-none' src='src\assets\btn-skip-right.png' />
                 </button>
                 </div>
-                <div className='flex flex-col md:flex-row justify-center px-12 md:-my-10 md:h-44'>
+                <div className='flex flex-col md:flex-row px-12 md:-my-10 md:h-44 md:mx-6'>
                     <button type='button' className='btn-answer flex-1'>
                         <span className='btn-answer-text'>
                             ONE
