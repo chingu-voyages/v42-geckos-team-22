9602a92ecc3a77457994660f232baa3dfee50006
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2023-01-19 16:05:37 -0700

    update

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -44,23 +44,23 @@ function FAQ() {
       <div className="lg:translate-y-30 absolute hidden h-40 w-5 translate-y-20 bg-purple md:block lg:h-60 lg:w-8"></div>
       <div className="absolute right-0 -z-20 hidden h-40 w-4/6 bg-light-orange md:block lg:h-60">
         <img
-          className="absolute left-44 w-28 lg:w-40 lg:left-96"
+          className="absolute left-44 w-28 lg:left-96 lg:w-40"
           src="src/assets/faqVector.svg"
           alt="faq vector"
         />
         <img
-          className="absolute right-0 -bottom-14 w-44 lg:w-60 lg:-bottom-20"
+          className="absolute right-0 -bottom-14 w-44 lg:-bottom-20 lg:w-60"
           src="src/assets/faqVector1.svg"
           alt="faq vector"
         />
       </div>
       <section className="md:pt-16 md:pb-16">
         <h2 className="text-7xl font-extrabold md:hidden">FAQ</h2>
-        <span className="hidden text-7xl font-extrabold md:block md:pl-14 md:pb-6 lg:text-9xl lg:pl-32">
+        <span className="hidden text-7xl font-extrabold md:block md:pl-14 md:pb-6 lg:pl-32 lg:text-9xl">
           Frequently
         </span>
-        <span className="hidden pl-36 text-5xl font-light md:block lg:text-7xl lg:pl-72 lg:pt-8">
-          asked questions
+        <span className="hidden pl-36 text-5xl font-light md:block lg:pl-72 lg:pt-8 lg:text-7xl">
+          asked question
         </span>
       </section>
       {content}
