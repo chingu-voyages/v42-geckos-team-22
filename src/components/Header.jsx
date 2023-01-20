94e5fd4f62fe92b71490d5ff2e527d69677cd7cb
eric <ericwang42@outlook.com>
2023-01-19 16:26:16 -0800

    feat:add nav styling/layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,22 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <header className="body-font bg-turquoise text-white">
-        <nav className="hidden md:flex md:flex-row">
-          <ul>
-            <NavLink>HOME</NavLink>
-            <NavLink>GAME TYPES</NavLink> //change to drop-down tabbed component
-            <NavLink>FAQ</NavLink>
-          </ul>
-          <button className="hidden md:inline-block">REGISTER</button>
-        </nav>
-      </header>
-    </div>
+    <header className="body-font  bg-turquoise bg-bg_nav text-xl font-bold text-white md:h-24 md:border-t-2 md:border-b-4 md:border-t-black md:border-b-black">
+      <nav className="hidden h-full md:mx-32 md:flex md:flex-row md:items-center md:justify-between">
+        <ul className="md:flex md:flex-row md:items-center md:gap-24">
+          <li>
+            <NavLink to="/">HOME</NavLink>
+          </li>
+          <li>
+            <NavLink>GAME TYPE</NavLink>
+          </li>
+          <li>
+            <NavLink to="/faq">FAQ</NavLink>
+          </li>
+        </ul>
+        <button className="hidden md:inline-block">REGISTER</button>
+      </nav>
+    </header>
   );
 }
 
