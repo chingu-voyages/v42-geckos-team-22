3417241a920d3b91c8f78883a734b60639169277
Elliot Erickson <elliot_erickson@protonmail.com>
2023-01-20 12:12:13 -0800

    Header Styling
    Added stripes via gradient, adjusted text size and color, logo size and color

--- src/components/GameComponents/GameHeading.jsx
+++ src/components/GameComponents/GameHeading.jsx
@@ -5,16 +5,18 @@ function GameHeading (props) {
 
     return (
         <div className={props.cname}>
-            <div className='border-2 border-black grow-1 bg-light-orange'>
+            <div className='border-2 border-black flex-grow-1 aspect-square bg-light-orange'>
                 User Profile
             </div>
-            <div className='border-2 border-black grow-4 bg-red-orange grid grid-columns-2'>
-                <div className='row-span-1'>
-                    <p className='text-8xl'>Flashy</p>
-                    <p className='text-6xl'>Flash Card</p>
+            <div className='border-2 border-black flex-grow-3 bg-red-orange flex justify-around header-stripes'>
+                <div className='font-bold text-white self-center'>
+                    <p className='text-8xl lg:text-9x1'>FLASHY</p>
+                    <p className='text-4xl'>FLASH CARD</p>
                 </div>
-                <div>
-                    <img src='src\assets\FlashyLogo.png'/>
+                <div className='self-center flex justify-end h-4/5'>
+                    <img 
+                        className=''
+                        src='src\assets\FlashyLogo.png'/>
                 </div>
             </div>
         </div>
