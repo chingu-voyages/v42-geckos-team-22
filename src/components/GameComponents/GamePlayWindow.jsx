a71aa0cf1aa7c3a0f97452bd172f1726fc019fba
Elliot Erickson <elliot_erickson@protonmail.com>
2023-01-23 14:10:37 -0800

    Game Page Margins
    Adjusted margins on game page for small screen to center the image

--- src/components/GameComponents/GamePlayWindow.jsx
+++ src/components/GameComponents/GamePlayWindow.jsx
@@ -5,8 +5,8 @@ function GamePlayWindow (props) {
 
     return (
         <div className={props.cname}>
-            <div className='m-1 md:flex-1 md:flex md:justify-center'>
-                <img className='md:ml-10' src='src\assets\card3.jpg' alt='Three' />
+            <div className='md:ml-14 md:flex-1 md:flex md:justify-center mt-14 md:mt-0'>
+                <img className='' src='src\assets\card3.jpg' alt='Three' />
             </div>
             <MoreOptions cname='hidden max-h-14 max-w-14 md:flex md:mr-1 md:mt-2'/>
         </div>
