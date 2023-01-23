45f484b11138497b77d0a30e1dfb12a84c60ef52
eric <ericwang42@outlook.com>
2023-01-22 22:01:05 -0800

    feat: footer style update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,18 +7,22 @@ import { RiInstagramFill } from "react-icons/ri";
 function Footer() {
   return (
     <footer className="w-screen bg-turquoise text-gray-300">
-      <div className="w-full pt-20 md:flex md:flex-row md:justify-around md:px-40">
-        <div className="md:flex md:flex-col">
-          <div className="mb-10 flex flex-col items-center justify-center gap-y-4">
-            <h2 className="text-lg font-bold uppercase">download</h2>
-            <figure>
+      <div className="w-full pt-20 md:flex md:flex-row md:justify-evenly md:px-40 md:pb-20">
+        <div className="md:mr-80 md:flex md:flex-col">
+          <div className="mb-10 flex flex-col items-center justify-center gap-y-4 md:items-start">
+            <h2 className="text-lg font-bold uppercase text-gray-200">
+              download
+            </h2>
+            <figure className="flex flex-row flex-wrap">
               <PlayStore />
               <AppStore />
             </figure>
           </div>
 
-          <div className="mb-10 flex flex-col items-center justify-center gap-y-4">
-            <h2 className="text-lg font-bold uppercase">connect</h2>
+          <div className="mb-10 flex flex-col items-center justify-center gap-y-4 md:mb-0 md:items-start">
+            <h2 className="text-lg font-bold uppercase text-gray-200">
+              connect
+            </h2>
             <div className="flex flex-row gap-x-3">
               <span>
                 <FaFacebookF className="socialIcons" />
@@ -38,24 +42,15 @@ function Footer() {
             </div>
           </div>
         </div>
-        <div className="mb-10 flex flex-col items-center justify-center gap-y-4">
-          <h2 className="text-lg font-bold uppercase">shop</h2>
-          <ul className="flex flex-col items-center justify-center">
-            <li className="footerLinks">Amazon</li>
-            <li className="footerLinks">Merch</li>
-            <li className="footerLinks">eBooks</li>
-            <li className="footerLinks">iTunes</li>
-          </ul>
-        </div>
 
-        <div className="flex flex-col items-center justify-center gap-y-4 pb-20">
-          <h2 className="text-lg font-bold uppercase">about</h2>
-          <ul className="flex flex-col items-center justify-center">
+        <div className="mb-10 flex flex-col items-center justify-center gap-y-4 md:mb-0 md:items-start">
+          <h2 className="text-lg font-bold uppercase text-gray-200">about</h2>
+          <ul className="flex flex-col items-center justify-center md:items-start">
             <li className="footerLinks">About Us</li>
             <li className="footerLinks">Careers</li>
             <li className="footerLinks">Terms of Use</li>
             <li className="my-4">
-              <span className="flex flex-col items-center justify-center">
+              <span className="flex flex-col items-center justify-center md:items-start">
                 <p>42 Developer Street</p>
                 <p>San Francisco, CA 94105</p>
               </span>
@@ -63,7 +58,18 @@ function Footer() {
             <li>+1 (415) - HIRE - MEEH</li>
           </ul>
         </div>
+
+        <div className="flex flex-col items-center justify-center gap-y-4 pb-20 md:items-start md:justify-start md:pb-0">
+          <h2 className="text-lg font-bold uppercase text-gray-200">shop</h2>
+          <ul className="flex flex-col items-center justify-center md:items-start">
+            <li className="footerLinks">Amazon</li>
+            <li className="footerLinks">Merch</li>
+            <li className="footerLinks">eBooks</li>
+            <li className="footerLinks">iTunes</li>
+          </ul>
+        </div>
       </div>
+
       <div className="flex flex-col items-center  justify-center bg-slate-800 bg-opacity-60 py-3 md:flex-row md:justify-between md:px-32">
         <p className="text-sm">
           © 2023 | Made with 🧡 & 🥑 by Team{" "}
