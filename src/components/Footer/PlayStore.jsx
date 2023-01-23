8400810ae1d210feb210c966e048a9f7b4e90cfa
eric <ericwang42@outlook.com>
2023-01-22 21:13:06 -0800

    update: add style update

--- src/components/Footer/PlayStore.jsx
+++ src/components/Footer/PlayStore.jsx
@@ -3,11 +3,11 @@ import { FaGooglePlay } from "react-icons/fa";
 
 export default function PlayStore() {
   return (
-    <button class="inline-flex items-center rounded-lg bg-gray-800 py-3 px-5 hover:bg-gray-700 hover:text-white focus:outline-none">
-      <FaGooglePlay />
-      <span class="ml-4 flex flex-col items-start leading-none">
-        <span class="mb-1 text-xs text-gray-400">GET IT ON</span>
-        <span class="title-font font-medium">Google Play</span>
+    <button className="inline-flex items-center rounded-lg bg-gray-300 py-3 px-5 text-turquoise hover:bg-gray-100 hover:font-extrabold focus:outline-none">
+      <FaGooglePlay className="text-lg font-bold" />
+      <span className="ml-4 flex flex-col items-start leading-none">
+        <span className="mb-1 text-xs text-turquoise">GET IT ON</span>
+        <span className="title-font font-bold">Google Play</span>
       </span>
     </button>
   );
