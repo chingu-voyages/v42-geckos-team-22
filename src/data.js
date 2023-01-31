d1b0f3c87dd8d88a46aef8e91d4a5c5680cf1880
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2023-01-31 09:05:41 -0700

    data schema example

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const dataJson = [
+  {
+    id: 1,
+    gameType: "Number", //use on homepage and nav for game type selection
+    games: [
+      {
+        img: "https://www.freepnglogos.com/uploads/1-number-png/cartoon-1-number-five-for-friday-time-beach-sand-3.png",
+        name: "Number 1",
+        options: ["one", "two", "three"],
+        correctAnswer: "one", //if this found in "options", user win
+      },
+      {
+        img: "https://e7.pngegg.com/pngimages/277/128/png-clipart-number-numerical-digit-mathematics-counting-multiplication-number-2-child-numerical-digit-thumbnail.png",
+        name: "Number 2",
+        options: ["one", "two", "three"],
+        correctAnswer: "two",
+      },
+    ],
+  },
+];
