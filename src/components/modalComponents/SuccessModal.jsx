4e10fc67443fd04b4bc60f09654fa9bf48d17110
eric <ericwang42@outlook.com>
2023-02-09 13:18:30 -0800

    fix: success modal ui

--- src/components/modalComponents/SuccessModal.jsx
+++ src/components/modalComponents/SuccessModal.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import mascot from "../../assets/modal/mascot.svg";
 import pawl from "../../assets/modal/pawl.svg";
 
 export default function SuccessModal({ toggleModal, showModal }) {
@@ -9,7 +8,7 @@ export default function SuccessModal({ toggleModal, showModal }) {
       {showModal && (
         <div
           onClick={toggleModal} // User clicks backdrop will close
-          className="fixed inset-0 z-50 flex cursor-pointer items-center justify-center bg-zinc-200 bg-opacity-40 backdrop-blur-[1.5px]"
+          className="fixed inset-0 z-50 flex items-center justify-center bg-zinc-200 bg-opacity-40 backdrop-blur-[1.5px]"
         >
           <div
             onClick={(e) => {
@@ -17,24 +16,19 @@ export default function SuccessModal({ toggleModal, showModal }) {
               // User clicks outside modal will close
               e.stopPropagation();
             }}
-            className="border-8 relative flex h-full w-full flex-col items-center justify-center rounded-xl border-white bg-light-orange text-white sm:h-4/6 sm:w-7/12 sm:max-w-md"
+            className="relative flex h-full w-full flex-col items-center justify-center gap-y-24 rounded-xl border-6 border-white bg-light-orange text-white sm:h-4/6 sm:max-w-sm sm:justify-between sm:gap-0"
           >
             <img
               src={pawl}
-              className="absolute right-6 top-6 z-50 w-12"
+              className="z-50 w-12 translate-x-32 cursor-pointer sm:mt-4 sm:mr-4 sm:translate-x-0 sm:self-end"
               alt="pawl"
               onClick={toggleModal}
             />
-            <img
-              src={mascot}
-              className="absolute bottom-14 w-4/5 max-w-md p-4"
-              alt="mascot"
-            />
-            <div className="absolute bottom-36 z-50 flex flex-col items-center justify-center text-center text-black">
-              <p className="text-2xl font-black">
+            <div className="z-50 flex h-[27rem] w-80 flex-col items-center justify-center bg-mascot bg-cover bg-center bg-no-repeat text-center text-black sm:mb-12">
+              <p className="pt-28 text-2xl font-black">
                 Account Successfully <br /> Registered
               </p>
-              <p className="mt-7 mb-5">Let's start learning</p>
+              <p className="mt-16 mb-5">Let's start learning</p>
               <Link
                 className="rounded-md border-3 border-white bg-orange py-2 px-7 font-semibold text-white"
                 onClick={toggleModal}
