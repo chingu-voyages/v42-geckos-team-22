7a20393c1363b89739958dbff43665aa6df9a9dc
Elliot Erickson <elliot_erickson@protonmail.com>
2023-02-11 13:58:17 -0800

    loading screen works

--- src/components/GameComponents/GameSpinnerWindow.jsx
+++ src/components/GameComponents/GameSpinnerWindow.jsx
@@ -6,11 +6,13 @@ function GameSpinnerWindow (props) {
     let circleCommonClasses = 'h-2.5 w-2.5 bg-red-orange rounded-full';
 
     return (
-       
-                    <div className="flex justify-center items-center">
+                    <div className="flex flex-col justify-center items-center mt-24">
+                    <p className="p-4 font-semibold">Loading...</p>
+                    <div className="flex">
                         <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
                         <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
                         <div className={`${circleCommonClasses} animate-bounce`}></div>
+                    </div>
                     </div>
                     
                     )
--- src/features/api/api.jsx
+++ src/features/api/api.jsx
@@ -2,33 +2,23 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import GameSpinnerWindow from "../../components/GameComponents/GameSpinnerWindow"
 
-
 const key = "33121417-d326c5dfd781e6d9400ae77ef";
-const imgID = 6812515;
 
 function GetApiPics({currentQuestionImage}) {
 
   let correctAnswer = useSelector(
     (state) => state.gameFunction.correctAnswer
   );
-
-//https://www.freecodecamp.org/news/fetch-data-react/
-//https://redux-toolkit.js.org/rtk-query/overview
-//https://www.geeksforgeeks.org/how-to-fetch-data-from-apis-using-asynchronous-await-in-reactjs/
-
-    console.log("in API.jsx", currentQuestionImage)
     
     const [image, setImage] = useState(null);
     const [loading, setLoading] = useState(false);
     
   const getPics = async () => {
     setLoading(true)
-    console.log("in Get pics loading 1", loading)
     const res = await fetch(`https://pixabay.com/api/?key=${key}&id=${currentQuestionImage}`);
     const data = await res.json();
     setImage(data.hits[0].largeImageURL);
     setLoading(false)
-    console.log("in Get pics loading 2", loading)
   };
 
   useEffect(() => {
